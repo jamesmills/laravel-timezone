dfdfc063b1a7d8f46a5af2117bfdfcf3679019ab
Amandio Magalhaes <amandio.magalhaes@gmail.com>
2020-01-10 11:56:12 -0800

    Keep it simple

--- src/LaravelTimezoneServiceProvider.php
+++ src/LaravelTimezoneServiceProvider.php
@@ -28,7 +28,12 @@ class LaravelTimezoneServiceProvider extends ServiceProvider
     public function boot()
     {
         // Allow migrations publish
-        $this->publishes($this->getMigrations(), 'migrations');
+        if (! class_exists('AddTimezoneColumnToUsersTable')) {
+            $timestamp = date('Y_m_d_His', time());
+            $this->publishes([
+                __DIR__.'/database/migrations/add_timezone_column_to_users_table.php.stub' => database_path("/migrations/{$timestamp}_add_timezone_column_to_users_table.php"),
+            ], 'migrations');
+        }
 
         // Register the Timezone alias
         AliasLoader::getInstance()->alias('Timezone', \JamesMills\LaravelTimezone\Facades\Timezone::class);
@@ -72,25 +77,4 @@ class LaravelTimezoneServiceProvider extends ServiceProvider
         $this->app->bind('timezone', Timezone::class);
     }
 
-    private function getMigrations(): array
-    {
-        $packageMigrationDir = __DIR__.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations';
-        $applicationMigrationFolder = database_path('migrations').DIRECTORY_SEPARATOR;
-        $baseTimestamp = date('Y_m_d_Hi');
-        $migrations = [];
-
-        /** @var SplFileInfo $splFileInfo */
-        foreach (File::files($packageMigrationDir) as $key => $splFileInfo) {
-            $pattern = '/(\d{4}_\d{2}_\d{2}_\d{6}_){1}(.*)/';
-            $subject = $splFileInfo->getFilename();
-            preg_match($pattern, $subject, $matches);
-            $filename = $matches[2];
-
-            $fileTimestamp = $baseTimestamp . str_pad($key, 2, '0', STR_PAD_LEFT);
-
-            $migrations[$splFileInfo->getPathname()] = $applicationMigrationFolder.$fileTimestamp."_".$filename;
-        }
-
-        return $migrations;
-    }
 }
