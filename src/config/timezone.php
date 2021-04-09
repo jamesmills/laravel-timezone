7b9c5e7694e209bef02cf6a736b2eb7172fc0d3c
sergotail <sergotail@mail.ru>
2021-04-09 17:27:40 +0300

    Refactored config file

--- src/config/timezone.php
+++ src/config/timezone.php
@@ -4,6 +4,74 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Default Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure default timezone for requests with no auth user info.
+    | If null specified, app.timezone config value will be used.
+    |
+    */
+
+    'default' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Overwrite Existing Timezone Default Value
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure if you would like to overwrite existing
+    | timezones if they have been already set in the database.
+    | Note that this is default value, per-user value checked for existence first.
+    | options [true, false]
+    |
+    */
+
+    'overwrite' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lookup Array
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the lookup array whom it will be used to fetch 
+    | the user remote address.
+    | When a key is found inside the lookup array that key it will be used.
+    |
+    */
+
+    'lookup' => [
+        'server' => [
+            'REMOTE_ADDR',
+        ],
+        'headers' => [],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | DateTime Default Format
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure if you would like to overwrite the
+    | default format.
+    |
+    */
+
+    'format' => 'jS F Y g:i:a',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Empty DateTime String
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure string to show when date is null.
+    | If null specified, string 'Empty' will be used.
+    |
+    */
+
+    'empty_date' => null,
+
+    /*
+    |--------------------------------------------------------------------------
     | Flash messages
     |--------------------------------------------------------------------------
     |
@@ -14,6 +82,33 @@ return [
     */
 
     'flash' => 'laravel',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lookup Array
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure flash messages for timezone setup results.
+    | If null specified, no message will be flashed for current case.
+    |
+    */
+
+    'messages' => [
+        'fail' => [
+            'key' => 'error',
+            'message' => 'We cannot determine your timezone',
+        ],
+
+        'default' => [
+            'key' => 'warning',
+            'message' => 'We cannot determine your timezone and have set it to default: %s',
+        ],
+
+        'success' => [
+            'key' => 'info',
+            'message' => 'We have set your timezone to %s',
+        ],
+    ],
 
     'timezone_check' => [
         /*
@@ -52,100 +147,5 @@ return [
         */
 
         'listener' => null,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Overwrite Existing Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure if you would like to overwrite existing
-    | timezones if they have been already set in the database.
-    | Note that this is default value, per-user value checked for existence first.
-    | options [true, false]
-    |
-    */
-
-    'overwrite' => true,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Overwrite Default Format
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure if you would like to overwrite the
-    | default format.
-    |
-    */
-
-    'format' => 'jS F Y g:i:a',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Empty Date 
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure string to show when date is null.
-    | If null specified, string 'Empty' will be used.
-    |
-    */
-
-    'empty_date' => null,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Default Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure default timezone for requests with no auth user info.
-    | If null specified, app.timezone config value will be used.
-    |
-    */
-
-    'default' => null,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Lookup Array
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the lookup array whom it will be used to fetch 
-    | the user remote address.
-    | When a key is found inside the lookup array that key it will be used.
-    |
-    */
-
-    'lookup' => [
-        'server' => [
-            'REMOTE_ADDR',
-        ],
-        'headers' => [],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Lookup Array
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure flash messages for timezone setup results.
-    | If null specified, no message will be flashed for current case.
-    |
-    */
-
-    'messages' => [
-        'fail' => [
-            'key' => 'error',
-            'message' => 'We cannot determine your timezone',
-        ],
-
-        'default' => [
-            'key' => 'warning',
-            'message' => 'We cannot determine your timezone and have set it to default: %s',
-        ],
-
-        'success' => [
-            'key' => 'info',
-            'message' => 'We have set your timezone to %s',
-        ],
     ],
 ];
