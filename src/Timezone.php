48d366f1b6213f98949b8576e8b22b00bb2fd066
Ahmad Al Desrahim <37100012+aldesrahim@users.noreply.github.com>
2021-05-19 03:46:17 +0700

    Update Timezone.php
    Change format to translatedFormat

--- src/Timezone.php
+++ src/Timezone.php
@@ -23,10 +23,10 @@ class Timezone
         $date->setTimezone($timezone);
 
         if (is_null($format)) {
-            return $date->format(config('timezone.format'));
+            return $date->translatedFormat(config('timezone.format'));
         }
 
-        $formatted_date_time = $date->format($format);
+        $formatted_date_time = $date->translatedFormat($format);
 
         if ($format_timezone) {
             return $formatted_date_time . ' ' . $this->formatTimezone($date);
