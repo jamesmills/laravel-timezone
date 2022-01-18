5d9f3511bacaba7477fe4bd16de5f716d149703a
Michal Borychowski <807297+boryn@users.noreply.github.com>
2022-01-18 10:34:12 +0100

    Take the first found IP from 'lookup' config key

--- src/Listeners/Auth/UpdateUsersTimezone.php
+++ src/Listeners/Auth/UpdateUsersTimezone.php
@@ -140,10 +140,9 @@ class UpdateUsersTimezone
         $value = null;
 
         foreach ($keys as $key) {
-            if (!request()->$type->has($key)) {
-                continue;
+            if (request()->$type->has($key)) {
+                return request()->$type->get($key);
             }
-            $value = request()->$type->get($key);
         }
 
         return $value;
