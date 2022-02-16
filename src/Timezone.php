df199f7e75a42f8f5392aac67cc05b95e2379d98
Al Desrahim <37100012+aldesrahim@users.noreply.github.com>
2022-02-16 11:45:03 +0700

    Added option to enable Carbon's translatedFormat  (#69)

--- src/Timezone.php
+++ src/Timezone.php
@@ -12,21 +12,23 @@ class Timezone
      * @param  bool  $format_timezone
      * @return string
      */
-    public function convertToLocal(?Carbon $date, $format = null, $format_timezone = false) : string
+    public function convertToLocal(?Carbon $date, $format = null, $format_timezone = false, $enableTranslation = null) : string
     {
         if (is_null($date)) {
             return __('Empty');
         }
 
         $timezone = (auth()->user()->timezone) ?? config('app.timezone');
-
+        
+        $enableTranslation = $enableTranslation !== null ? $enableTranslation : config('timezone.enableTranslation');
+        
         $date->setTimezone($timezone);
 
         if (is_null($format)) {
-            return $date->format(config('timezone.format'));
+            return $enableTranslation ? $date->translatedFormat(config('timezone.format')) : $date->format(config('timezone.format'));
         }
 
-        $formatted_date_time = $date->format($format);
+        $formatted_date_time = $enableTranslation ? $date->translatedFormat($format) : $date->format($format);
 
         if ($format_timezone) {
             return $formatted_date_time . ' ' . $this->formatTimezone($date);
