106be08a5f2f9c703b538683b8ca4d7739d765b5
Grant Holle <hollegrant@gmail.com>
2023-02-20 13:31:53 +0400

    Add missing facade method

--- src/Facades/Timezone.php
+++ src/Facades/Timezone.php
@@ -4,10 +4,12 @@ namespace JamesMills\LaravelTimezone\Facades;
 
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
+use Carbon\FactoryImmutable;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static string getCurrentTimezone()
+ * @method static FactoryImmutable getCarbonFactory($user)
  * @method static string|CarbonImmutable convertToLocal(null|Carbon|CarbonImmutable $date, string $format = null)
  * @method static string formatLocal(null|Carbon|CarbonImmutable $date, string $format = null)
  * @method static CarbonImmutable convertFromLocal(mixed $date)
